7ada97005b41778ef7dc78bf7b650e2dc4cd3d58
Thegaijin <semaganda@outlook.com>
2017-07-12 22:30:17 +0300

    Implementation of login and sign up features with flask

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 from app import app
+from app.models.bucketlistApp import BucketlistApp
 from .forms import RegistrationForm, LoginForm
 from flask import Flask, flash, redirect, render_template, request, session, abort
 
-'''import models'''
 import os
+
+bucketlistapp = BucketlistApp()
 
 
 @app.route('/')
@@ -15,7 +17,7 @@ def index():
         return render_template('login.html', form=form)
 
 
-@app.route('/')
+'''@app.route('/')
 @app.route('/index')
 @app.route('/login', methods=['POST'])
 def login():
@@ -35,14 +37,43 @@ def login():
             else:
                 return "user doesn't exist"
     else:
-        return "form not validated"
+        return "form not validated"'''
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        log_in = bucketlistapp.login(username, password)
+        if log_in == "Successfully logged in":
+            return render_template('lists.html')
+        elif log_in == 'Your username and password combination does not exist, please try again':
+            flash(
+                'Your username and password combination does not exist, please try again')
+            return render_template('login.html')
+        elif log_in == 'Your username does not exist':
+            return render_template('signup.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def register():
-    form = RegistrationForm()
-    if request.method == 'GET':
-        return render_template('signup.html', form=form)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm-password']
+        sign_up = bucketlistapp.create_user(
+            username, password, confirm_password)
+        if sign_up == 'Welcome to the Bucketlist app, please login':
+            return render_template('login.html')
+        elif sign_up == "Your username already exists":
+            return render_template('login.html')
+        elif sign_up == "Please make sure all the fields are filled in":
+            return render_template('signup.html')
+    else:
+        return render_template('signup.html')
 
 
 @app.route('/lists')
